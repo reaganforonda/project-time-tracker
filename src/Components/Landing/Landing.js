1ec3795d6c14b49bd00d748444056bda418e7aa6
reaganforonda <foronda.reagan@gmail.com>
2018-04-23 23:00:25 -0600

    landing page styling

--- src/Components/Landing/Landing.js
+++ src/Components/Landing/Landing.js
@@ -8,23 +8,38 @@ export default class Landing extends React.Component {
   render() {
     return (
       <div className="landing-page">
-        <nav>
-          <h1>Creative App Title</h1>
-          <a href={process.env.REACT_APP_LOGIN}>
-            <button>Login</button>
-          </a>
-        </nav>
-        <header className="landing-header" />
+        <header className="landing-header">
+          <nav>
+            <h1>Creative App Title</h1>
+            <a href={process.env.REACT_APP_LOGIN}>
+              <button>Login</button>
+            </a>
+          </nav>
+        </header>
+
         <section className="about-section-landing">
           <h2>About</h2>
-          <p>
-            Lorem ipsum dolor amet meh selfies DIY pour-over selvage iceland,
+          <p className='about-text'>
+            Lorem ipsum dolor amet fashion axe iceland vape single-origin coffee
+            try-hard photo booth keffiyeh hexagon food truck church-key VHS palo
+            santo gentrify. Crucifix enamel pin waistcoat, keytar semiotics
+            ramps ennui. Migas art party put a bird on it, sustainable forage
+            direct trade tattooed thundercats helvetica vaporware taiyaki. Fanny
+            pack gastropub neutra migas, man braid pabst church-key polaroid
+            gluten-free chia. Fanny pack ugh before they sold out mustache
+            post-ironic chia direct trade man bun meditation locavore. Man bun
+            gochujang bicycle rights actually kitsch activated charcoal
+            farm-to-table deep v stumptown banjo flannel bespoke tote bag
+            fingerstache. <p>Oh. You need a little dummy text for your mockup? How
+            quaint.</p>
           </p>
         </section>
-        <footer>
-          <div>
 
-          </div>
+        <section className='section-two'>
+        </section>
+        
+        <footer>
+          <div />
         </footer>
       </div>
     );
