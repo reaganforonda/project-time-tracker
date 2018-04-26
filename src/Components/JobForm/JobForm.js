8e5ef00e0ae7e561084755e78c07c45f7e586a98
reaganforonda <foronda.reagan@gmail.com>
2018-04-25 21:39:20 -0600

    bug fix

--- src/Components/JobForm/JobForm.js
+++ src/Components/JobForm/JobForm.js
@@ -1,8 +1,17 @@
 import React from "react";
 import axios from "axios";
 import ContentAdd from "material-ui/svg-icons/content/add";
-import { Dialog, TextField, DatePicker, RaisedButton, FloatingActionButton, MenuItem, Snackbar, SelectField } from "material-ui";
-import MenuItemCustom from "../MenuItem/MenuItem";
+import {
+  Dialog,
+  TextField,
+  DatePicker,
+  RaisedButton,
+  FloatingActionButton,
+  MenuItem,
+  Snackbar,
+  SelectField
+} from "material-ui";
+
 
 export default class JobForm extends React.Component {
   constructor(props) {
@@ -15,7 +24,7 @@ export default class JobForm extends React.Component {
       startDate: null,
       hourlyRate: 0,
       clients: [],
-      client: ''
+      client: ""
     };
 
     this.handleOpenModal = this.handleOpenModal.bind(this);
@@ -24,6 +33,10 @@ export default class JobForm extends React.Component {
     this.formatDate = this.formatDate.bind(this);
     this.handleGetClients = this.handleGetClients.bind(this);
     this.handleClientSelect = this.handleClientSelect.bind(this);
+    this.handleAddJob = this.handleAddJob.bind(this);
+
+    this.handleOnConfirm = this.handleOnConfirm.bind(this);
+    this.handleResetState = this.handleResetState.bind(this);
   }
 
   componentDidMount() {
@@ -31,13 +44,7 @@ export default class JobForm extends React.Component {
   }
 
   handleCancelModalClick() {
-    this.setState({
-      modalOpen: false,
-      jobName: "",
-      jobDescription: "",
-      startDate: null,
-      hourlyRate: 0
-    });
+this.handleResetState();
   }
 
   handleTextChange(e) {
@@ -54,10 +61,12 @@ export default class JobForm extends React.Component {
 
   // Formate Datepicker's date to something for useable for postgres sql
   formatDate(date) {
-    let formatedDate = `${date.getMonth()+1}/${date.getDate()}/${date.getFullYear()}`;
+    let formatedDate = `${date.getMonth() +
+      1}/${date.getDate()}/${date.getFullYear()}`;
     return formatedDate;
   }
 
+  // TODO: Change this so it only get clients related to the logged in User
   handleGetClients() {
     axios
       .get("http://localhost:3005/api/clients")
@@ -69,20 +78,55 @@ export default class JobForm extends React.Component {
       });
   }
 
-  handleClientSelect(event, index, value) {
+  handleClientSelect = (event, index, value) => {
     this.setState({ client: value });
-    console.log(this.state.client);
+  };
+
+  handleAddJob(){
+    let job = {
+      client_id : this.state.client,
+      //TODO: Change this so that it's actually the user logged in
+      // TODO: Look at completed ? Should i be auto set to false?
+      user_id : 1,
+      job_name : this.state.jobName,
+      start_date : this.state.startDate,
+      completed : false,
+      rate : this.state.hourlyRate,
+      description : this.state.jobDescription
+    }
+
+    axios.post(`http://localhost:3005/api/job`, (job)).then((result) => {
+      console.log(result.data);
+    }).catch((e) => {
+      console.log(`${e}`)
+    })
+  }
+
+  handleOnConfirm(){
+    this.handleAddJob();
+    this.setState({modalOpen : false});
+    this.handleResetState();
+  }
+
+  handleResetState(){
+    this.setState({
+      modalOpen: false,
+      jobName: "",
+      jobDescription: "",
+      startDate: null,
+      hourlyRate: 0,
+      client: ""
+    })
   }
 
   render() {
     let clientArr = this.state.clients.map(client => {
       return (
-        <div key={client.client_id}>
-          <MenuItemCustom
+          <MenuItem
+            key={client.client_id}
             primaryText={client.client_name}
             value={client.client_id}
           />
-        </div>
       );
     });
 
@@ -95,15 +139,19 @@ export default class JobForm extends React.Component {
         >
           <ContentAdd />
           <Dialog modal={true} open={this.state.modalOpen}>
+            <SelectField
+              hintText="Select Client"
+              floatingLabelText="Select Client"
+              value={this.state.client}
+     
+              onChange={(event, index, value) =>
+                this.handleClientSelect(event, index, value)
+              }
+              
+            >
+              {clientArr}
+            </SelectField>
             <form className="job-entry-form">
-
-              <SelectField
-                hintText="Select Client"
-                floatingLabelText="Select Client"
-                value={this.state.client} onChange={(event, index, value)=>this.handleClientSelect(event, index, value)}>
-                {clientArr}
-              </SelectField>
-
               <TextField
                 value={this.state.jobName}
                 onChange={e => this.handleTextChange(e)}
@@ -143,12 +191,11 @@ export default class JobForm extends React.Component {
                   onClick={() => this.handleCancelModalClick()}
                 />
 
-                <RaisedButton label="CONFIRM" primary={true} />
+                <RaisedButton label="CONFIRM" primary={true} onClick={()=>this.handleOnConfirm()}/>
               </div>
             </form>
           </Dialog>
         </FloatingActionButton>
-
       </div>
     );
   }
