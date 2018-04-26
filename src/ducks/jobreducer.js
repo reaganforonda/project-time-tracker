85a290a04ad9153171ec01858d391333416d2bc3
reaganforonda <foronda.reagan@gmail.com>
2018-04-26 09:27:54 -0600

    work on job reducer

--- src/ducks/jobreducer.js
+++ src/ducks/jobreducer.js
@@ -1,20 +1,49 @@
-import axios from 'axios';
+import axios from "axios";
 
 const JOB_INITIAL_STATE = {
-    job_id : 0,
-    client_id : 0,
-    user_id : 0,
-    job_name : '',
-    start_date : '',
-    end_date : '',
-    completed : false,
-    rate : 0,
-    description : ''
+  job_id: 0,
+  client_id: 0,
+  user_id: 0,
+  job_name: "",
+  start_date: "",
+  end_date: "",
+  completed: false,
+  rate: 0,
+  description: "",
+  jobs: []
+};
+
+const GET_ALL_JOBS = "GET_ALL_JOBS";
+const ADD_JOB = "ADD_JOB";
+
+export function getAllJobs() {
+  let jobsData = axios
+    .get("/api/jobs")
+    .then(jobs => {
+      return jobs.data;
+    })
+    .catch(e => {
+      console.log(`Error: ${e}`);
+    });
+
+  return {
+    type: GET_ALL_JOBS,
+    payload: jobsData
+  };
 }
 
-export default function jobreducer(state = JOB_INITIAL_STATE, action){
-    switch(action.type) {
-        default :
-        return state;
-    }
+export function addJob(){
+
+}
+
+export default function jobReducer(state = JOB_INITIAL_STATE, action) {
+  switch (action.type) {
+      case GET_ALL_JOBS:
+      let obj = Object.assign({}, state, {jobs : action.payload});
+      return (
+          obj.jobs
+      ) 
+    default:
+      return state;
+  }
 }
--- src/store.js
+++ src/store.js
@@ -1,12 +1,12 @@
 import {createStore, applyMiddleware, combineReducers} from 'redux';
 import userReducer from './ducks/userReducer';
-import jobreducer from './ducks/jobreducer';
+import jobReducer from './ducks/jobreducer';
 import clientReducer from './ducks/clientReducer';
 import promiseMiddleware from 'redux-promise-middleware';
 
 const reducer = combineReducers({
     userReducer : userReducer,
-    jobReducer : jobreducer,
+    jobReducer : jobReducer,
     clientReducer : clientReducer
 })
 
